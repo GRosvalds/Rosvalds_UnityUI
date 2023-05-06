e90365bbcc50c6ec332e2dc2e8550a4d915bd54b
Gabriels Rosvalds <gabriels.rosvalds@gmail.com>
2023-05-06 17:59:46 +0300

    Pievienoti apģērbi patrikam, un uzvelkot drēbes tās piefiksējās konkrētā vietā

--- Assets/Scripts/DragAndDrop2.cs
+++ Assets/Scripts/DragAndDrop2.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class DragAndDrop2 : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler {
+public class DragAndDrop2 : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler, IDropHandler {
 
 	private RectTransform rectTransform;
 	private Image image;
+	private CanvasGroup canvasGroup;
+
+	[SerializeField] private Canvas Canvas;
 	#region IDragHandler implementation
 	public void OnDrag (PointerEventData eventData)
 	{
-		rectTransform.anchoredPosition += eventData.delta;
+		rectTransform.anchoredPosition += eventData.delta/ Canvas.scaleFactor;
 
 	}
 	#endregion
@@ -21,6 +24,7 @@ public class DragAndDrop2 : MonoBehaviour, IDragHandler, IBeginDragHandler, IEnd
 	public void OnBeginDrag (PointerEventData eventData)
 	{
 		image.color = new Color32 (255, 255, 255, 10);
+		canvasGroup.blocksRaycasts = false;
 	}
 
 	#endregion
@@ -30,12 +34,23 @@ public class DragAndDrop2 : MonoBehaviour, IDragHandler, IBeginDragHandler, IEnd
 	public void OnEndDrag (PointerEventData eventData)
 	{
 		image.color = new Color32 (255, 255, 255, 0);
+		canvasGroup.blocksRaycasts = true;
 	}
+
+	#region IDropHandler implementation
+
+	public void OnDrop (PointerEventData eventData)
+	{
+		throw new System.NotImplementedException ();
+	}
+
+	#endregion
 
 	#endregion
 	void Start () {
 		rectTransform = GetComponent<RectTransform>();
 		image = GetComponent<Image> ();
+		canvasGroup = GetComponent<CanvasGroup> ();
 	}
 
 
